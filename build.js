549eaa09c317298a79540c5ec75138e7bf4e7fa2
CNine <cnine229@gmail.com>
2024-10-05 01:10:04 +0800

    chore(build): bundle root json

--- build.js
+++ build.js
@@ -207,6 +207,23 @@ async function processScript(filePath) {
   await fs.writeFile(destination, code);
 }
 
+async function processRootJson(filePath) {
+  const source = await fs.readFile(filePath, 'utf8');
+
+  const jsonObject = JSON.parse(source);
+
+  const exportStatement =
+    '"use strict";exports.__esModule = true;exports.default=' +
+    JSON.stringify(jsonObject) +
+    ';';
+
+  const destination = filePath
+    .replace('src', 'dist')
+    .replace(/\.json$/, '.json.js');
+  await fs.copy(filePath, destination);
+  await fs.writeFile(destination, exportStatement);
+}
+
 async function processTemplate(filePath) {
   const destination = filePath
     .replace('src', 'dist')
@@ -253,6 +270,10 @@ const cb = async (filePath) => {
   if (filePath.endsWith('.css')) {
     await processStyle(filePath);
     return;
+  }
+
+  if (/src\/[^/]+\.json$/.test(filePath)) {
+    await processRootJson(filePath);
   }
 
   await fs.copy(filePath, filePath.replace('src', 'dist'));
